305b777a3a5a82ad9604c5ec1c506eae8dda0aa2
White Excellent <whiteexcellent@gmail.com>
2026-02-13 03:01:44 +0300

    Fix: Remove all output directory configs, use Vercel defaults

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove 'output: export' to enable API routes
-  // This runs on Vercel's serverless functions
-  distDir: '.next',
   images: {
     unoptimized: true,
   },
